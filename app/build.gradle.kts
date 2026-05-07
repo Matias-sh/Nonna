78240a13c1c04eb4a64438ec242f23907c71fbfe
Matias-sh <devmatiasbritez@gmail.com>
2026-05-07 12:20:09 -0300

    nueva version release bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.cocido.nonna.free"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
