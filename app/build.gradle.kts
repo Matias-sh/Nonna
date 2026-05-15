1851c1c786c35c28aa64ca253df12530ec6b704f
Matias-sh <devmatiasbritez@gmail.com>
2026-05-15 16:07:39 -0300

    nonna1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.cocido.nonna"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.1.8"
+        versionCode = 10
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
